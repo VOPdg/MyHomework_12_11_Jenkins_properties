5a95741a973f9ff93bca832165cfd8bbefd3d9ba
veronikapodgol <veronika.podgol@gmail.com>
2022-05-21 16:53:32 +0300

    add credentials owner

--- src/test/java/properties/OwnerTests.java
+++ src/test/java/properties/OwnerTests.java
@@ -1,14 +1,18 @@
 package properties;
 
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class OwnerTests {
+
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
     @Test
     @Tag("owner")
     void loginTest() {
-        String login = "";
-        String password = "";
+        String login = config.login();
+        String password = config.password();
 
         System.out.println("login"+ login);
         System.out.println("password"+ password);
